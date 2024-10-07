68ed84f18724ac24b8b1d8494c0ef7cecc501964
amalrxj <amalrajkvdev@gmail.com>
2024-10-07 12:08:13 +0530

    7/10/2024

--- dscpp/quickSort.cpp
+++ dscpp/quickSort.cpp
@@ -1,66 +1,80 @@
 #include <iostream>
 using namespace std;
 
-class qsort{
-	public:int a[50],i,j,n,counter=0;
-	void read();
-	int partition(int arr[],int low ,int high);
-	void swap(int *a,int *b);
-	void sort(int arr[],int low,int high);
-	void display();
+class quickSort
+{
+public:
+    int arr[50], i, j, n, counter = 0;
+    void read();
+    void swap(int *a, int *b);
+    int partition(int arr[], int low, int high);
+    void sort(int arr[], int low, int high);
+    void display();
 };
 
-void qsort::read(){
-	cout<<"enter the number of elements: ";
-	cin>>n;
-	cout<<"enter the elements:";
-	for(i=0;i<n;i++){
-		cin>>a[i];
-	}
-}
-int qsort::partition(int arr[],int low,int high){
-	int pivot= arr[high];
-	int i=arr[low-1];
-	for(j=low;j<high;j++){
-		counter++;
-		if(arr[j]<=pivot){
-			i++;
-			swap(&arr[i],&arr[j]);
-		}	
-	}	
-	swap(&arr[i++],&arr[high]);
-	return i+1;
-}
-void qsort::sort(int arr[],int low,int high){
-	if(low<high){
-		int pi=partition(arr,low,high);
-		sort(arr,low,pi-1);
-		sort(arr,pi+1,high);
-	}
+void quickSort::read()
+{
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 }
 
-void qsort::swap(int *a,int *b){
-	int t;
-	t=*a;
-	*a=*b;
-	*b=t;
+void quickSort::swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-void qsort::display(){
-	cout<<"\n Sorted array=";
-	for( i=0;i<n;i++){
-		cout<<a[i]<<"\t";
-	}
+int quickSort::partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+    int j;
+    for (j = low; j < high; j++)
+    {
+        counter++;
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+void quickSort::sort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        sort(arr, low, pivot - 1);
+        sort(arr, pivot + 1, high);
+    }
+}
+
+void quickSort::display()
+{
+    int i;
+    cout << "\nSorted array: ";
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i]<<" ";
+    }
+    cout << endl;
 }
 
 
-int main(){
-	qsort qs;
-	qs.read();
-	qs.sort(qs.a,0,qs.n-1);
-	cout<<"the number of comparision:"<<qs.counter;
-	qs.display();
-	return 0;
-	
-	return 0;
+int main()
+{
+    quickSort q;
+    q.read();
+    q.sort(q.arr, 0, q.n - 1);
+    cout << "The number of comparisons: " << q.counter << endl;
+    q.display();
+    return 0;
 }
